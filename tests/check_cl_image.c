f6c0d59847485d8eea20d1a30bf118549606d72d
Sora <sora@mougnou.fr>
2013-03-03 14:40:50 +0100

    Added tests for histogram generation

--- tests/check_cl_image.c
+++ tests/check_cl_image.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 #include <check.h>
 #include <cl_image.h>
+#include <cl_histogram.h>
 #include <image_utils.h>
 
-void
-setup (void)
-{
+void setup (void) {
 }
 
-void
-teardown (void)
-{
+void teardown (void) {
 }
 
-START_TEST (test_near_power)
-{
+START_TEST (test_near_power) {
   int counter = searchNearestPower(4);
   fail_unless(counter == 2, "Got %i", counter);
   counter = searchNearestPower(512);
@@ -45,46 +41,56 @@ unsigned char one_fill(int x, int y, int c) {
   return 1;
 }
 
-START_TEST (test_thumbnail)
-{
+void assertFloatEquals(float expected, float res) {
+  fail_unless(res == expected, "Expected %f, got %f", expected, res);
+}
+
+START_TEST (test_histogram_simple) {
   int width = 32;
   int height = 32;
   int resultWidth;
   int resultHeight;
 
-  unsigned char ** results = malloc(sizeof(unsigned char **));
+  float ** results = malloc(sizeof(float **));
   unsigned char ** pixels = malloc(sizeof(unsigned char **));
   fill_pixels(&zero_fill, height, width, pixels);
   for(int i = 0; i < width * height * RGB_CHANNEL; i++) {
     fail_unless((*pixels)[i] == 0);
   }
-  processImage(pixels, 32, 32, &resultWidth, &resultHeight, results);
 
-  fail_unless(resultWidth == 1, "Result width : %d", resultWidth);
-  fail_unless(resultHeight == 1, "Result height : %d", resultHeight);
+  cl_struct clStruct = initCl("src/kernel_image.cl", "generateHistogram");
+  generateHistogram(clStruct, pixels, 32, 32
+      , &resultWidth, &resultHeight, results);
+
+  ck_assert_int_eq(resultWidth, 1);
+  ck_assert_int_eq(resultHeight, 1);
+
   for(int c = 0; c < RGB_CHANNEL; c++) {
-    fail_unless((*results)[c] == 0, "results[%i]==%u"
-        , c, (*results)[c]);
+    assertFloatEquals((*results)[c * BUCKET_NUMBER], 1.f);
+    for(int i = 1; i < BUCKET_NUMBER; i++){
+      assertFloatEquals((*results)[c * BUCKET_NUMBER + i], 0.f);
+    }
   }
-  fill_pixels(&one_fill, height, width, pixels);
 }
 END_TEST
 
-Suite *
-soragl_suite (void)
-{
+START_TEST (test_histogram_different_values) {
+}
+END_TEST
+
+
+Suite * soragl_suite (void) {
   Suite *s = suite_create ("cl_image");
   TCase *tc_core = tcase_create ("cl_image");
   tcase_add_checked_fixture (tc_core, setup, teardown);
   tcase_add_test (tc_core, test_near_power);
-  tcase_add_test (tc_core, test_thumbnail);
+  tcase_add_test (tc_core, test_histogram_simple);
+  tcase_add_test (tc_core, test_histogram_different_values);
   suite_add_tcase (s, tc_core);
   return s;
 }
 
-int
-main (void)
-{
+int main (void) {
   int number_failed;
   Suite *s = soragl_suite ();
   SRunner *sr = srunner_create (s);
