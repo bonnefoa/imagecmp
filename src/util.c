92e842ef0fc087287d95a248b0a4c366ef5163bf
Sora <anthonin.bonnefoy@gmail.com>
2013-04-07 16:23:23 +0200

    Fix stupid sizeof error...

--- src/util.c
+++ src/util.c
@@ -4,7 +4,7 @@ char *strdup (const char *s)
 {
         char *res;
         int numChars = strlen(s) + 1;
-        if((res = malloc(sizeof(numChars))) == NULL)
+        if((res = malloc(sizeof(char) * numChars)) == NULL)
                 return NULL;
         strncpy(res,s, numChars);
         return res;
